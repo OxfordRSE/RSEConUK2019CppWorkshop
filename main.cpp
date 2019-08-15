35fd0fd2130897206374fe9b76773c4f2f3217e5
Fergus Cooper <fcooper8472@gmail.com>
2019-08-15 12:04:35 +0100

    Use some std filesystem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+
+  auto build_dir = fs::canonical(fs::path{"."});
+  fs::create_directories(build_dir / "output");
+
   return 0;
 }
