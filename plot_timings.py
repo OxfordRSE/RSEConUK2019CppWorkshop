f2d554a01fd08af2e317a3da8879a2037a443339
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2019-09-16 15:58:24 +0100

    Update script to plot timings

--- plot_timings.py
+++ plot_timings.py
@@ -3,19 +3,47 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
+if len(sys.argv)<2:
+    sys.exit("plot_timings.py: missing operand\n"
+             "Usage: python plot_timings.py checkpoint1 [checkpoint2 ...]")
 for i in range(len(sys.argv)-1):
-    df = pd.read_csv("checkpoint_{:d}/timings.csv".format(checkpoint1))
-    reduced_df = df[["checkpoint","reduce","transform_reduce"]]
-    reduced_df['checkpoint'] = [sys.argv[i+1]]*df.shape[0]
-
-    if(i>0):
-        fulldf = pd.concat(full_df, reduced_df)
-    else:
-        fulldf = reduced_df
+    checkpoint = int(sys.argv[i+1])
+    try:
+        full_df = pd.read_csv("checkpoint_{:d}/timings.csv".format(checkpoint))
+    except:
+        sys.exit("Error: Could not find file checkpoint_{:d}/timings.csv".format(checkpoint))
         
-print(fulldf)
-# melted_df = pd.melt(reduced_df, id_vars="checkpoint", var_name="operation", value_name="time")
+    full_df["checkpoint"] = [checkpoint]*full_df.shape[0]
+    if(i>0):
+        df = pd.concat([df, full_df], ignore_index=True)
+    else:
+        df = full_df
 
-# ax = sns.barplot(x="operation", y="time", hue="checkpoint", data=melted_df)
 
-# plt.show()
+fig_ax = []
+fig = plt.figure()
+gs = fig.add_gridspec(2,3)
+fig_ax1 = fig.add_subplot(gs[0,:])
+fig_ax1.set_title("any_of, reduce and transform_reduce")
+fig_ax2 = fig.add_subplot(gs[1,0])
+fig_ax2.set_title("read_data")
+fig_ax3 = fig.add_subplot(gs[1,1])
+fig_ax3.set_title("nth_element")
+fig_ax4 = fig.add_subplot(gs[1,2])
+fig_ax4.set_title("mismatch")
+allaxes = fig.get_axes()
+
+things2plot = [["checkpoint","any_of","reduce","transform_reduce"],
+               ["checkpoint", "read_data"],
+               ["checkpoint", "nth_element"],
+               ["checkpoint", "mismatch"]]
+
+for i in range(len(things2plot)):
+    reduced_df = df[things2plot[i]]        
+    melted_df = pd.melt(reduced_df, id_vars="checkpoint", var_name="operation", value_name="time")
+
+    allaxes[i].set_title("haha")
+    sns.barplot(x="operation", y="time", hue="checkpoint", data=melted_df, ax=allaxes[i])
+
+
+plt.show()
