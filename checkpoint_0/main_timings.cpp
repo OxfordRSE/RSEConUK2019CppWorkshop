4f589bed4ee6203491c8fcef7e8ac0557a864b1d
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2019-09-17 23:07:32 +0100

    Update main with timings for checkpoints 0 and 7

--- checkpoint_0/main_timings.cpp
+++ checkpoint_0/main_timings.cpp
@@ -1,162 +1,163 @@
-#include <iostream>
+#include <chrono>
+#include <cmath>
 #include <cstdlib>
 #include <fstream>
+#include <iostream>
+#include <iterator>
 #include <vector>
-#include <cmath>
-#include <chrono>
-
-// An optimized version of Bubble Sort 
-void bubbleSort(std::vector<double> &v) 
-{ 
-   int i, j;
-   double temp;
-   bool swapped; 
-   for (i = 0; i < v.size()-1; i++) 
-   { 
-     swapped = false; 
-     for (j = 0; j < v.size()-i-1; j++) 
-     { 
-        if (v[j] > v[j+1]) 
-        {
-	  temp = v[j];
-	  v[j]=v[j+1];
-	  v[j+1]=temp;
-           swapped = true; 
-        } 
-     } 
-  
-     // IF no two elements were swapped by inner loop, then break 
-     if (swapped == false) 
-        break; 
-   } 
-}
 
 int main()
 {
-  
-  // Create a new directory under the build directory
-  std::string output_dir = "./output";
-  std::string mkdir_command = "mkdir "+output_dir;
-  // System takes a const char* as a parameter
-  system(mkdir_command.c_str());
-
-  // Create an output file
-  std::ofstream output_file(output_dir+"/results.dat");
-  output_file << "blahhhhhhh";
-
-  std::string data_files[3];
-  data_files[0] = "../data/0_rse_workshop.dat";
-  data_files[1] = "../data/1_rse_workshop.dat";
-  data_files[2] = "../data/2_rse_workshop.dat";
-
+  // Declare start and end points for timings with chrono
   std::chrono::high_resolution_clock::time_point t1;
   std::chrono::high_resolution_clock::time_point t2;
   
-  // Read first data file
-  t1 = std::chrono::high_resolution_clock::now();
-  std::ifstream f(data_files[0]);
-  // Read the file and fill a vector with the numbers
-  std::vector<double> v;
-  double a;
-  while (!f.eof()){
-    f >> a;
-    v.push_back(a);
-  }
-  f.close();
-  t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> readData = t2 - t1;
-  
-  // Compute mean
-  t1 = std::chrono::high_resolution_clock::now();
-  double sum=0;
-  for (int i=0;i<v.size();i++){
-    sum+=v[i];
-  }
-  const double mean=sum/v.size();
-  t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> reduce = t2 - t1;
-  
-  // Compute standard deviation
-  t1 = std::chrono::high_resolution_clock::now();
-  sum=0;
-  for (int i=0;i<v.size();i++){
-    sum+=v[i]*v[i];
-  }
-  const double var = sum/v.size() - mean*mean;
-  t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> transform_reduce = t2 - t1;
-  
-  // Compute variance
-  const double std = sqrt(var);
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 1. Reading some data in from a file
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  auto tt1 = std::chrono::high_resolution_clock::now();
-  
-  // Compute skewness
-  sum=0;
-  double one_over_sigma3 = 1./(std*std*std);
-  for (int i=0;i<v.size();i++){
-    sum+=(v[i]-mean)*(v[i]-mean)*(v[i]-mean)*one_over_sigma3;
-  }
-  const double skew = sum/v.size();
-  
-  // Compute median
-  t1 = std::chrono::high_resolution_clock::now();
-  std::vector<double> v2{v};
-  bubbleSort(v2);
-  int half_way = v2.size()/2;
-  const double median = v2[half_way];
-  t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> nth_element = t2 - t1;
+    // Find some pre-prepared data files
+    std::vector<std::string> data_files;
+    data_files.push_back("../data/0_rse_workshop.dat");
+    data_files.push_back("../data/1_rse_workshop.dat");
+    data_files.push_back("../data/2_rse_workshop.dat");
 
-  auto tt2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> ms = tt2 - tt1;
-  std::cout << std::fixed << "sequential took " << ms.count() << " ms\n";
-
-  std::cout << "mean:   " << mean << '\n';
-  std::cout << "var:    " << var << '\n';
-  std::cout << "skew:   " << skew << '\n';
-  std::cout << "median: " << median << '\n';
-
-  // Any of the elements > 50?
-  t1 = std::chrono::high_resolution_clock::now();
-  // for (int i=0;i<v.size();i++){
-  //   if(v[i]>50){
-  //     std::cout << v[i] << " ";}
-  // }
-  std::cout << std::endl;
-  t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> any_of = t2 - t1;
-
-  t1 = std::chrono::high_resolution_clock::now();
-  int dist=0;
-  double first, second;
-  for (int i=0;i<v.size()-1;i++)
+    // Do something with each of the files...
+    for (int i = 0; i < data_files.size(); ++i)
     {
-      if(abs(v[i+1]-v[i])>2*std)
-	{
-	  dist = i;
-	  first = v[i];
-	  second = v[i+1];
-	  break;
-	}
+        std::cout << data_files[i] << std::endl;
     }
-  t2 = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> mismatch = t2 - t1;
-  if(dist){
-    std::cout << "Position " << dist << ", first " << first << " second: " << second << '\n';
-  }
-  else{
-    std::cout << "Could not find any valid position" << std::endl;
-  }
 
-  std::ofstream timings("timings.csv", std::ios::app);
-  timings << std::fixed << readData.count() << ", "
-	  << reduce.count() << ", "
-	  << transform_reduce.count() << ", "
-	  << nth_element.count() << ", "
-	  << any_of.count() << ", "
-	  << mismatch.count() << std::endl;
-  timings.close();
-  
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 2. Calculate the mean and standard deviation of some data
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Read some data in
+    std::ifstream f(data_files[0]);
+    std::vector<double> v{std::istream_iterator<double>(f), std::istream_iterator<double>()};
+
+    // Compute mean
+    t1 = std::chrono::high_resolution_clock::now();
+    double sum = 0.0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        sum += v[i];
+    }
+    const double mean = sum / v.size();
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> reduce = t2 - t1;
+    
+    // Compute standard deviation
+    t1 = std::chrono::high_resolution_clock::now();
+    sum = 0.0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        sum += v[i] * v[i];
+    }
+    const double var = sum / v.size() - mean * mean;
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> transform_reduce = t2 - t1;
+    
+    // Compute variance
+    const double std = std::sqrt(var);
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 3. Calculate the skewness - there's no default algorithm for that!
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Compute skewness
+    sum = 0.0;
+    double one_over_sigma3 = 1.0 / (std * std * std);
+    for (int i = 0; i < v.size(); i++)
+    {
+        sum += (v[i] - mean) * (v[i] - mean) * (v[i] - mean) * one_over_sigma3;
+    }
+    const double skew = sum / v.size();
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 4. Calculate the median (and bonus chrono)
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    
+
+    // Take a copy of the vector so we can sort it
+    std::vector<double> v2 = v;
+    t1 = std::chrono::high_resolution_clock::now();
+    std::sort(v2.begin(), v2.end());
+    int half_way = v2.size() / 2;
+    const double median = v2[half_way];
+
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> nth_element = t2 - t1;
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 5. What other algorithms are there?
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Are any of the elements > 50?
+    t1 = std::chrono::high_resolution_clock::now();
+    bool any_greater_than_50 = false;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i] > 50.0)
+        {
+            any_greater_than_50 = true;
+        }
+    }
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> any_of = t2 - t1;
+
+    // First position where consecutive elements differ by more than twice the standard deviation
+    t1 = std::chrono::high_resolution_clock::now();
+    int dist = 0;
+    double first, second;
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        if (std::fabs(v[i + 1] - v[i]) > 2 * std)
+        {
+            dist = i;
+            first = v[i];
+            second = v[i + 1];
+            break;
+        }
+    }
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> mismatch = t2 - t1;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 7. Writing some data back out to a csv file - pulling several concepts together
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    std::string output_dir = "./output";
+    std::string mkdir_command = "mkdir -p " + output_dir;
+    // System takes a const char* as a parameter
+    system(mkdir_command.c_str());
+
+    // Create an output file
+    t1 = std::chrono::high_resolution_clock::now();
+    std::ofstream output_file(output_dir + "/results.dat");
+    output_file << v[0];
+    for (int i = 1; i < v.size(); i++)
+    {
+        output_file << ',' << v[i];
+    }
+    output_file.close();
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> writeData = t2 - t1;
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// Writing timings to csv file checkpoint_xx/timings.csv
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::ofstream timings("timings.csv", std::ios::app);
+    timings << std::fixed << readData.count() << ", "
+	    << reduce.count() << ", "
+	    << transform_reduce.count() << ", "
+	    << nth_element.count() << ", "
+	    << any_of.count() << ", "
+	    << mismatch.count() << std::endl;
+    timings.close();
 }
-
--- checkpoint_7/main_timings.cpp
+++ checkpoint_7/main_timings.cpp
@@ -1,54 +1,26 @@
+#include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <cstdlib>
 #include <execution>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <numeric>
-#include <random>
 #include <vector>
 
 namespace fs = std::filesystem;
 
-void generate_data()
-{
-    int num_to_generate = 3;
-
-    std::random_device rd;
-    std::mt19937 gen{rd()};
-
-    std::uniform_real_distribution real_dist_mean(10.0, 60.0);
-    std::uniform_real_distribution real_dist_std(3.0, 8.0);
-
-    for (int i = 0; i < num_to_generate; ++i)
-    {
-        std::normal_distribution<double> norm(real_dist_mean(gen), real_dist_std(gen));
-
-        std::ofstream f("/home/fergus/GitRepos/OxfordRSE/OxfordRSEProjects/RSEConUK2019CppWorkshop/data/" +
-                        std::to_string(i) + "_rse_workshop.dat");
-
-        f << norm(gen);
-        for (int j = 0; j < 1000; ++j)
-        {
-            f << ' ' << norm(gen);
-        }
-        f.close();
-    }
-}
-
 int main()
 {
-
-    // Create a new directory under the build directory
-    auto build_dir = fs::canonical(fs::path{"."});
-    auto output_dir = build_dir / "output";
-    fs::create_directories(output_dir);
-
-    // Create an output file
-    auto output_file = output_dir / "results.dat";
-    std::ofstream(output_file) << "blahhhhhhh";
-
-    //  generate_data();
+  // Declare start and end points for timings with chrono
+  std::chrono::high_resolution_clock::time_point t1;
+  std::chrono::high_resolution_clock::time_point t2;
+  
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 1. Reading some data in from a file
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Find all files like *_rse_workshop.dat under a specific directory
     std::vector<fs::path> data_files;
@@ -60,80 +32,109 @@ int main()
         }
     }
 
+    // This is just to make sure the same file is first, so the numbers later are the same on each version.
+    // The sort isn't necessary!
     std::sort(data_files.begin(), data_files.end());
 
     // Which data files did we find? (range-for loop)
-    for (auto &d : data_files)
+    for (const auto &d : data_files)
     {
-        std::cout << d << '\n';
+        std::cout << d << std::endl;
     }
-    
-    auto t1 = std::chrono::high_resolution_clock::now();
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 2. Calculate the mean and standard deviation of some data
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Read some data in
     std::ifstream f(data_files[0]);
     std::vector<double> v{std::istream_iterator<double>(f), std::istream_iterator<double>()};
-    auto t2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> readData = t2 - t1;
-
-    auto t1 = std::chrono::high_resolution_clock::now();
-    const double mean = std::reduce(v.begin(), v.end(), 0.0) / v.size();
-    auto t2 = std::chrono::high_resolution_clock::now();
+    
+    t1 = std::chrono::high_resolution_clock::now();
+    const double mean = std::reduce(std::execution::par, v.begin(), v.end(), 0.0) / v.size();
+    t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> reduce = t2 - t1;
 
-    auto t1 = std::chrono::high_resolution_clock::now();
-    const double var = std::transform_reduce(v.begin(), v.end(), v.begin(), 0.0) / v.size() - mean * mean;
-    auto t2 = std::chrono::high_resolution_clock::now();
+    t1 = std::chrono::high_resolution_clock::now();
+    const double var =
+        std::transform_reduce(std::execution::par_unseq, v.begin(), v.end(), v.begin(), 0.0) / v.size() - mean * mean;
+    t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> transform_reduce = t2 - t1;
+    
     const double std = std::sqrt(var);
 
-    // Lambdas
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 3. Calculate the skewness - there's no default algorithm for that!
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    auto tt1 = std::chrono::high_resolution_clock::now();
-
+    // Compute skewness
     auto third_moment = [mean, std](const double x) { return std::pow((x - mean) / std, 3); };
-    const double skew = std::transform_reduce(v.begin(), v.end(), 0.0, std::plus<>(), third_moment) / v.size();
+    const double skew =
+        std::transform_reduce(std::execution::par, v.begin(), v.end(), 0.0, std::plus<>(), third_moment) / v.size();
 
-    std::vector<double> v2{v};
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 4. Calculate the median (and bonus chrono)
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Take a copy of the vector so we can sort it
+    std::vector<double> v2 = v;
+
+    t1 = std::chrono::high_resolution_clock::now();
     auto half_way = v2.size() / 2;
-
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::nth_element(v2.begin(), std::next(v2.begin(), half_way), v2.end());
+    std::nth_element(std::execution::par, v2.begin(), std::next(v2.begin(), half_way), v2.end());
     const double median = v2.at(half_way);
-    auto t2 = std::chrono::high_resolution_clock::now();
+    t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> nth_element = t2 - t1;
-    
-    auto tt2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> ms = tt2 - tt1;
-    std::cout << std::fixed << "sequential took " << ms.count() << " ms\n";
 
-    std::cout << "mean:   " << mean << '\n';
-    std::cout << "var:    " << var << '\n';
-    std::cout << "skew:   " << skew << '\n';
-    std::cout << "median: " << median << '\n';
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 5. What other algorithms are there?
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Any of the elements > 50?
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::cout << std::any_of(v.begin(), v.end(), [](const double x) { return x > 50.0; }) << '\n';
-    auto t2 = std::chrono::high_resolution_clock::now();
+    t1 = std::chrono::high_resolution_clock::now();
+    const bool any_greater_than_50 =
+        std::any_of(std::execution::unseq, v.begin(), v.end(), [](const double x) { return x > 50.0; });
+    t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> any_of = t2 - t1;
-    
+
     // First position where consecutive elements differ by more than twice the standard deviation
-    auto t1 = std::chrono::high_resolution_clock::now();
-    auto threshold = [std](const double x, const double y) { return std::fabs(x - y) < 2.0 * std; };
-    auto answer = std::mismatch(v.begin(), std::prev(v.end()), std::next(v.begin()), threshold);
-    auto dist = std::distance(v.begin(), answer.first);
-    auto t2 = std::chrono::high_resolution_clock::now();
+    t1 = std::chrono::high_resolution_clock::now();
+    auto comparison = [std](const double x, const double y) { return std::fabs(x - y) < 2.0 * std; };
+    const auto answer = std::mismatch(v.begin(), std::prev(v.end()), std::next(v.begin()), comparison);
+
+    const auto dist = std::distance(v.begin(), answer.first);
+    t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> mismatch = t2 - t1;
-    
-    std::cout << "Position " << dist << ", first " << *answer.first << " second: " << *answer.second << '\n';
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 7. Writing some data back out to a csv file - pulling several concepts together
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    std::ofstream timings("timings.csv", ios::app);
+    // Create a new directory under the build directory
+    auto build_dir = fs::canonical(fs::path{"."});
+    auto output_dir = build_dir / "output";
+    fs::create_directories(output_dir);
+
+    // Create an output file
+    t1 = std::chrono::high_resolution_clock::now();
+    std::ofstream output_file(output_dir / "results.dat");
+    output_file << std::accumulate(v.begin(), v.end(), std::to_string(v[0]),
+        [](std::string a, double b) { return std::move(a) + ',' + std::to_string(b); });
+    output_file.close();
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> writeData = t2 - t1;
+
+        ////////////////////////////////////////////////////////////////////////////////
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// Writing timings to csv file checkpoint_xx/timings.csv
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::ofstream timings("timings.csv", std::ios::app);
     timings << std::fixed << readData.count() << ", "
 	    << reduce.count() << ", "
 	    << transform_reduce.count() << ", "
 	    << nth_element.count() << ", "
 	    << any_of.count() << ", "
-	    << mismatch.count() << ", "
-	    << std::endl;
-    return 0;
+	    << mismatch.count() << std::endl;
+    timings.close();
 }
